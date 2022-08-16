7bf86f797f6fe11fb7d2d33722deff0a1228911e
Bulmaro de la Cruz <bulma0324@gmail.com>
2022-08-16 17:53:41 -0500

    Nueva practica

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.composeapp
+import android.content.Intent
 import android.content.res.Configuration
 import android.media.Image
 import android.os.Bundle
@@ -14,9 +15,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,6 +25,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
@@ -47,189 +48,29 @@ override fun onCreate(savedInstanceState: Bundle?) {
 
 }
 @Composable
-fun text (text:String){
-    Text(text = text)
-}
-@Composable
-fun price (text:String){
-    Text(text = text, fontWeight = FontWeight.Bold, fontSize = 15.sp)
-}
-@Composable
-fun imageObjects(@DrawableRes image: Int){
-    Box(
-
-        Modifier
-            .size(
-                width = 100.dp,
-                height = 100.dp,
-            ),
-        contentAlignment = Alignment.Center
-
-
-    )
-    {
-        Image(
-            painter = painterResource(image),
-            "Imagen",
-            Modifier
-                .size(90.dp)
-                .clip(shape = CutCornerShape(2.dp))
-                .background(Color.LightGray))
-
-    }
-}
-@Composable
-fun icons(@DrawableRes image: Int){
-
-    Image(
-        painter = painterResource(image),
-        "Imagen",
-        Modifier.size(10.dp),
-
-    )
-
-}
-
-@Composable
 fun Components() {
-    Column(
+    val context = LocalContext.current
+
+    Button(
+
+        onClick = {
+            //Obtener el context
+            val new_page = Intent(context,MainActivity2::class.java)
+            context.startActivity(new_page)
+        },
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green)
 
     ) {
-        Row(
 
-            horizontalArrangement = Arrangement.Start
-        ) {
-            price(text = "MY CHART")
-
-        }
-        Box(
-
-            contentAlignment = Alignment.TopCenter
-        ) {
-            Column(
-                Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.SpaceAround,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-
-                Divider()
-                Spacer(Modifier.size(20.dp))
-                RowComponent(R.drawable.pet, "T-Shirt Pet", "24.00")
-                Spacer(Modifier.size(20.dp))
-                Divider()
-                Spacer(Modifier.size(20.dp))
-                RowComponent(R.drawable.tshirt,"T-Shirt Flash", "20.00")
-                Spacer(Modifier.size(20.dp))
-                Divider()
-                Spacer(Modifier.size(20.dp))
-                RowComponent(R.drawable.shirt,"T-Shirt Arcoiris", "23.00")
-                Spacer(Modifier.size(20.dp))
-                Divider()
-                Spacer(Modifier.size(20.dp))
-            }
-
-        }
-        Box(
-            Modifier.fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ){
-            Row(
-                Modifier.fillMaxWidth().padding(horizontal = 20.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                price(text = "Total")
-                price(text = "$67.00")
-
-            }
-        }
+        Text(text = "Send")
     }
 
 }
-@Composable
-fun RowComponent(image: Int, nombre:String, precio:String){
-    Row(
-        Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceEvenly
-    ) {
-        //Imagen
-        imageObjects(image = image)
-        Column(
-            Modifier
-                .padding(10.dp),
-                //.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-
-        ) {
-            //Nombre
-            text(text = nombre)
-            price(text = precio)
-
-            Row(
-                Modifier.size(width = 50.dp, height = 15.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                icons(R.drawable.minus)
-                text(text = "1")
-                icons(R.drawable.plus)
-            }
-
-        }
-    }
-}
-@Preview(showSystemUi = true)
+@Preview()
 @Composable
 fun Preview() {
     Components()
 }
-/*    Row(
-        //horizontalArrangement = Arrangement.SpaceAround,
-        //verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Row(
-            Modifier
-                .size(height = 250.dp, width = 500.dp)
-                .background(Color.Yellow),
-        ) {
-            Column(
-                Modifier
-                    .size(height = 200.dp, width = 500.dp),
-                verticalArrangement = Arrangement.SpaceAround,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                text(text = "Bienvenido", Color.Blue)
-                text(text = "Como estas?", Color.Green)
-                text(text = "Portate bien", Color.Magenta)
-            }
-        }
-    }
-}*/
-/*@Composable
-fun columnInitial(){
-    Column(
-
-        //Reparte el espacio entre los elementos dentro de la columna
-        //verticalArrangement = Arrangement.SpaceEvenly
-
-        //El primer y ultimo elemento no tienen espacio en los bordes y el elemento de en medio centrado
-        //verticalArrangement = Arrangement.SpaceBetween
-
-        //Permite dejar espacios arriba y abajo  en los bordes pero dejando solo a la mitad
-        //verticalArrangement = Arrangement.SpaceAround,
-
-        //Centra los elementos de la columa
-        //horizontalAlignment = Alignment.CenterHorizontally,
-        //verticalArrangement = Arrangement.SpaceAround
-
-
-    ) {
-        text(text = "Bienvenido", Color.Blue)
-        text(text = "Como estas?", Color.Green)
-        text(text = "Portate bien", Color.Magenta)
-    }
-
-}*/
 
 
 
---
+++ app/src/main/java/com/example/composeapp/MainActivity2.kt
@@ -0,0 +1,60 @@
+package com.example.composeapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composeapp.ui.theme.ComposeAppTheme
+
+class MainActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+                val context = LocalContext.current
+
+                Button(
+
+                    onClick = {
+                        //Obtener el context
+                        val new_page = Intent(context,MainActivity::class.java)
+                        context.startActivity(new_page)
+                    },
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green)
+
+                ) {
+
+                    Text(text = "Send")
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeAppTheme {
+        Greeting("Android")
+    }
+}
