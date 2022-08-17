98acc15a213b365b67ad09cfd7a1239e38ad4366
Bulmaro de la Cruz <bulma0324@gmail.com>
2022-08-17 15:35:32 -0500

    Examen

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -13,14 +13,18 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
@@ -30,6 +34,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -38,38 +43,199 @@ import com.example.composeapp.ui.theme.ComposeAppTheme
 import com.example.composeapp.ui.theme.componente
 import org.intellij.lang.annotations.JdkConstants
 
+val gray = Color(	0xFFE6E6E6)
+val azul = Color(	0xFF8904B1)
+
+
 class MainActivity : ComponentActivity() {
 override fun onCreate(savedInstanceState: Bundle?) {
      super.onCreate(savedInstanceState)
      setContent {
-            Components()
+            ViewOne()
         }
     }
 
 }
+
 @Composable
-fun Components() {
+fun ViewOne() {
+
+   Column(
+       Modifier
+           .fillMaxSize()
+           .padding(vertical = 30.dp, horizontal = 15.dp)
+           .clip(RoundedCornerShape(30.dp))
+
+   ) {
+       Column(
+
+           Modifier
+               .padding(0.dp)
+               .background(azul),
+
+
+       ){
+
+           Row(
+               Modifier
+                   .fillMaxWidth()
+                   .size(height = 60.dp, width = 0.dp)
+                   .padding(10.dp),
+               horizontalArrangement = Arrangement.SpaceAround
+               ) {
+               ImageIcons(image = R.drawable.plus)
+               textNormal(text = "Mis permisos")
+               ImageIcons(image = R.drawable.mayorque)
+
+               }
+           Divider()
+           Row(
+               Modifier
+                   .fillMaxWidth()
+                   .size(height = 60.dp, width = 0.dp)
+                   .padding(10.dp),
+               horizontalArrangement = Arrangement.SpaceAround
+           ) {
+               ImageIcons(image = R.drawable.pencil)
+               textNormal(text = "Mis actividades")
+               ImageIcons(image = R.drawable.mayorque)
+
+           }
+           Divider()
+           Row(
+               Modifier
+                   .fillMaxWidth()
+                   .size(height = 60.dp, width = 0.dp)
+                   .padding(10.dp),
+               horizontalArrangement = Arrangement.SpaceAround
+           ) {
+               ImageIcons(image = R.drawable.download)
+               textNormal(text = "Mis calificaciones")
+               ImageIcons(image = R.drawable.mayorque)
+
+
+           }
+           Divider()
+
+       }
+       Spacer(Modifier.size(height = 20.dp, width = 10.dp))
+       Row(
+           Modifier.clip(RoundedCornerShape(20.dp))
+       ) {
+           Column(
+
+           ) {
+               Row(
+                   Modifier
+                       .background(azul)
+                       .fillMaxWidth()
+                       .padding(10.dp),
+                   verticalAlignment = Alignment.CenterVertically
+               ) {
+                   ImageStart(image = R.drawable.asuka)
+                   Spacer(Modifier.size(height = 10.dp, width = 10.dp))
+                   Column() {
+                       textTitle(text = "Gina Laurence")
+                       textNormal(text = "De catorce años de edad, nacida el 4 de diciembre de 2001")
+                   }
+               }
+               Row(
+                   Modifier
+                       .background(azul)
+                       .fillMaxWidth()
+                       .padding(10.dp),
+                   verticalAlignment = Alignment.CenterVertically
+               ) {
+                   ImageStart(image = R.drawable.rei)
+                   Spacer(Modifier.size(height = 10.dp, width = 10.dp))
+                   Column() {
+                       textTitle(text = "Rei Ayanami")
+                       textNormal(text = "Rei Ayanami es una enigmática figura cuyo inusual comportamiento confunde a sus compañeros.")
+                   }
+               }
+           }
+
+       }
+
+   }
+}
+@Composable
+fun textName(text:String){
+    Text(text = text,
+        color = Color.White,
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+@Composable
+fun textDescription(text:String){
+    Text(
+        text = text,
+        color = Color.White,
+        fontSize = 10.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Justify
+    )
+}
+@Composable
+fun textTitle(text:String){
+    Text(text = text,
+        color = Color.White,
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+@Composable
+fun textNormal(text:String){
+    Text(text = text,
+        color = Color.White,
+        fontSize = 20.sp
+
+    )
+}
+@Composable
+fun ImageStart(@DrawableRes image:Int){
+    Image(
+        painterResource(image),
+        "back",
+        Modifier
+            .clip(RoundedCornerShape(30.dp))
+            .size(40.dp)
+
+    )
+}
+@Composable
+fun ImageIcons(@DrawableRes image:Int){
+    Image(
+        painterResource(image),
+        "back",
+        Modifier.size(30.dp)
+
+
+
+    )
+}
+@Composable
+fun BottomNavigator(){
     val context = LocalContext.current
-
     Button(
-
         onClick = {
             //Obtener el context
             val new_page = Intent(context,MainActivity2::class.java)
             context.startActivity(new_page)
         },
-        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green)
-
+        Modifier
+            .size(width = 200.dp, height = 50.dp)
+            .clip(RoundedCornerShape(20.dp)),
+        colors = ButtonDefaults.buttonColors(backgroundColor = azul)
     ) {
-
-        Text(text = "Send")
+        Text(text = "Next Page", color = Color.White)
     }
-
 }
-@Preview()
+@Preview(showSystemUi= true)
 @Composable
 fun Preview() {
-    Components()
+    ViewOne()
 }
 
 
--- app/src/main/java/com/example/composeapp/MainActivity2.kt
+++ app/src/main/java/com/example/composeapp/MainActivity2.kt
@@ -2,59 +2,193 @@ package com.example.composeapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Scroller
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeapp.ui.theme.ComposeAppTheme
 
 class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-                val context = LocalContext.current
+        ViewTwo()
+    }
+}
+@Composable
+fun ViewTwo(){
+    Column(
+        Modifier
+            .background(gray)
+            .fillMaxSize()
+            .padding(20.dp),
 
-                Button(
-
-                    onClick = {
-                        //Obtener el context
-                        val new_page = Intent(context,MainActivity::class.java)
-                        context.startActivity(new_page)
-                    },
-                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green)
-
-                ) {
-
-                    Text(text = "Send")
-                }
-
+    ) {
+        Row(
+            Modifier.clip(RoundedCornerShape(10.dp))
+        ) {
+            Row(
+                Modifier
+                    .background(azul)
+                    .fillMaxWidth()
+                    .clip(CircleShape),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                textTitle(text = "Characters")
             }
+        }
+        Spacer(Modifier.size(height = 10.dp, width = 10.dp))
+        Row(
+            Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+
+        ) {
+            BoxPerson(
+                image = R.drawable.asuka,
+                "Asuka Langley",
+                "De catorce años de edad, nacida el 4 de diciembre de 20012 con nacionalidad estadounidense pero criada en Alemania lugar de fabricación de la unidad 02 antes de ser enviada a Japón. Su lengua primaria es el alemán.",
+                3)
+            BoxPerson(
+                image = R.drawable.rei,
+                "Rei Ayanami",
+                "Rei Ayanami es una enigmática figura cuyo inusual comportamiento confunde a sus compañeros. A medida que avanza la trama de la serie, comienza a verse más relacionada con la gente que la rodea, particularmente con su compañero de clase y piloto de Eva, Shinji Ikari.",
+                5)
+
+        }
+        Spacer(Modifier.size(20.dp))
+        Row (
+            Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ){
+            BottomNavigator()
+        }
+            
+    }
+}
+@Composable
+fun BoxPerson(@DrawableRes image:Int, name:String,description:String,rank: Int){
+
+    Box(
+        Modifier.clip(RoundedCornerShape(20.dp))
+    ) {
+        Box(
+            Modifier
+                .size(width = 150.dp, height = 400.dp)
+                .background(azul)
+                .padding(10.dp),
+            contentAlignment = Alignment.Center
+
+        ) {
+
+            Box() {
+                Column(
+                    Modifier
+                        .fillMaxSize()
+                        .padding(vertical = 10.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+
+
+                    ) {
+                    textName(text = name)
+                    Spacer(Modifier.size(10.dp))
+                    ImagesPersons(image)
+                    rowStars(rank = rank)
+                    Spacer(Modifier.size(10.dp))
+                    textDescription(text = description)
+                    Spacer(Modifier.size(10.dp))
+                    Row(
+                        Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ) {
+                        ImagesIcons(image = R.drawable.pencil)
+                        ImagesIcons(image = R.drawable.download)
+                    }
+                }
+            }
+        }
+
+    }
+}
+@Composable
+fun rowStars(rank:Int){
+    Row() {
+        if (rank == 3){
+            Image(painterResource(id = R.drawable.hexagramyellow), contentDescription = "starts")
+            Image(painterResource(id = R.drawable.hexagramyellow), contentDescription = "starts")
+            Image(painterResource(id = R.drawable.hexagramyellow), contentDescription = "starts")
+            Image(painterResource(id = R.drawable.hexagramgray), contentDescription = "starts")
+            Image(painterResource(id = R.drawable.hexagramgray), contentDescription = "starts")
+
+        }
+        else {
+            Image(painterResource(id = R.drawable.hexagramyellow), contentDescription = "starts")
+            Image(painterResource(id = R.drawable.hexagramyellow), contentDescription = "starts")
+            Image(painterResource(id = R.drawable.hexagramyellow), contentDescription = "starts")
+            Image(painterResource(id = R.drawable.hexagramyellow), contentDescription = "starts")
+            Image(painterResource(id = R.drawable.hexagramyellow), contentDescription = "starts")
+
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ImagesPersons(@DrawableRes image:Int){
+    Image(
+         painterResource(image),
+        "persons",
+        Modifier
+            .clip(RoundedCornerShape(10.dp))
+            .size(width = 100.dp, height = 100.dp)
+            .shadow(elevation = 30.dp, shape = AbsoluteCutCornerShape(0.dp))
+        )
 }
+    @Composable
+    fun ImagesIcons(@DrawableRes image:Int){
+        Image(
+            painterResource(image),
+            "icons",
+            Modifier.size(30.dp)
 
-@Preview(showBackground = true)
+        )
+    }
+    @Composable
+    fun BottomNavigator(){
+        val context = LocalContext.current
+        Button(
+            onClick = {
+                //Obtener el context
+                val new_page = Intent(context,MainActivity::class.java)
+                context.startActivity(new_page)
+            },
+            Modifier
+                .size(width = 200.dp, height = 50.dp)
+                .clip(RoundedCornerShape(20.dp)),
+            colors = ButtonDefaults.buttonColors(backgroundColor = azul)
+        ) {
+            Text(text = "Return", color = Color.White)
+        }
+    }
+@Preview()
 @Composable
 fun DefaultPreview() {
     ComposeAppTheme {
-        Greeting("Android")
+            ViewTwo()
+        }
     }
 }
