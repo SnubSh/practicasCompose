2ca76452a534ce0e06007ff060f5c7c5f4357b22
Bulmaro de la Cruz <bulma0324@gmail.com>
2022-10-08 17:12:37 -0500

    08-10-2022

--- app/src/main/java/com/example/composeapp/model/MainActivityModel.kt
+++ app/src/main/java/com/example/composeapp/model/MainActivityModel.kt
@@ -7,14 +7,58 @@ import androidx.lifecycle.ViewModel
 
 class MainActivityModel :ViewModel() {
 
-private val _counter = mutableStateOf(0)
+    private val _counter = mutableStateOf(0)
     val counter:State<Int> = _counter
 
-fun add(){
+    private val _text = mutableStateOf("")
+    val text:State<String> = _text
+
+    private val _textDropDown = mutableStateOf("")
+    val textDropDown:State<String> = _textDropDown
+
+    private val _lapiz = mutableStateOf(0)
+    val lapiz:State<Int> = _lapiz
+
+    private val _lapicero = mutableStateOf(0)
+    val lapicero:State<Int> = _lapicero
+
+    private val _borrador = mutableStateOf(0)
+    val borrador:State<Int> = _borrador
+
+    private val _cuaderno = mutableStateOf(0)
+    val cuaderno:State<Int> = _cuaderno
+
+    private val _result = mutableStateOf(0)
+    val result:State<Int> = _result
+
+
+    fun setString(msg:String){
+        _text.value = msg
+    }
+    fun setStringDropDown(msg:String){
+        _textDropDown.value = msg
+    }
+
+    fun setLapiz(){
+        _lapiz.value = 10
+    }
+    fun setLapicero(){
+        _lapicero.value = 15
+    }
+    fun setBorrador(){
+        _borrador.value = 5
+    }
+    fun setCuaderno(){
+        _cuaderno.value = 30
+    }
+    fun setSum(value:Int){
+        _result.value = _result.value + value
+    }
+    fun add(){
     _counter.value = _counter.value + 1
 }
     fun reset(){
-        _counter.value =0
+        _result.value = 0
     }
     fun sumar(){
         _counter.value=_counter.value+2
--- app/src/main/java/com/example/composeapp/view/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/view/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.composeapp.view
 import MainActivityModel
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composeapp.R
 
 //val gray = Color(	0xFFE6E6E6)
 //val azul = Color(	0xFF8904B1)
@@ -19,87 +29,186 @@ import androidx.compose.ui.tooling.preview.Preview
 
 
 class MainActivity : ComponentActivity() {
-    val ovm:MainActivityModel by viewModels()
+    val ovm: MainActivityModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-     super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
 
-     setContent {
+        setContent {
             Column(
-                Modifier.fillMaxWidth(),
+                Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding(all = 30.dp)
+                    ,
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                string()
-                ViewPractice()
-                ButtonResetValue()
-                ButtonDecreaseValue()
-                ButtonIncrementValueFor2()
-                ButtonIncrementFor5Value()
 
+            ) {
+//                string()
+//                ViewPractice()
+//                ButtonDecreaseValue()
+//                ButtonIncrementValueFor2()
+//                ButtonIncrementFor5Value()
+                imagen()
+                Spacer(Modifier.height(10.dp))
+                generarSpinner(ovm)
+            Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "Total: " + ovm.result.value.toString() +"  MXN")
+                ButtonResetValue()
+                }
             }
         }
     }
+@Composable
+fun imagen(){
+    Row(
+        Modifier.clip(shape = RoundedCornerShape(20.dp))
+    ) {
+        Image(painterResource(id = R.drawable.coin),"")
 
+    }
+}
 
     @Composable
     fun ViewPractice() {
 
-        Button(onClick = {ovm.add()}) {
+        Button(onClick = { ovm.add() }) {
             Text(text = "Sumar + 1")
-            
+
         }
 
     }
+
     @Composable
     fun string() {
-        Text(text = "Valor es: "+ovm.counter.value.toString())
-
+        Text(text = "Valor es: " + ovm.counter.value.toString())
 
 
     }
+
     @Composable
-    fun ButtonResetValue(){
-        Button(onClick = { ovm.reset() } ) {
+    fun ButtonResetValue() {
+        Button(onClick = { ovm.reset() }) {
             Text(text = "Reiniciar")
 
         }
     }
+
     @Composable
-    fun ButtonDecreaseValue(){
-        Button(onClick = {  ovm.restar()} ) {
+    fun ButtonDecreaseValue() {
+        Button(onClick = { ovm.restar() }) {
             Text(text = "Restar")
 
         }
     }
+
     @Composable
-    fun ButtonIncrementValueFor2(){
-        Button(onClick = {  ovm.sumarFor2()} ) {
+    fun ButtonIncrementValueFor2() {
+        Button(onClick = { ovm.sumarFor2() }) {
             Text(text = "incrementar + 2")
 
         }
     }
+
     @Composable
-    fun ButtonIncrementFor5Value(){
-        Button(onClick = { ovm.sumarFor5() } ) {
+    fun ButtonIncrementFor5Value() {
+        Button(onClick = { ovm.sumarFor5() }) {
             Text(text = "incrementar + 5")
 
         }
     }
 
+//    @Composable
+//    fun textFiel() {
+////        var textTemp = remember {
+////            mutableStateOf(0)
+////        }
+//
+//
+////        Text(text = "Value String: " + ovm.text.value.toString())
+////
+////        Button(onClick = { ovm.setString(textTemp.value.toString()) }) {
+////            Text(text = "Obten string")
+////
+////        }
+//
+//
+//
+//
+//    }
+
+    @Composable
+    fun generarSpinner(ovm: MainActivityModel) {
+
+        var expanded by remember { mutableStateOf(false) }
+
+        Box {
+            Button(onClick = { expanded = !expanded }) {
+                Text("Opciones")
+                Icon(
+                    imageVector = Icons.Filled.ArrowDropDown,
+                    contentDescription = null,
+                )
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = true },
+            ) {
+
+                DropdownMenuItem(onClick = {
+                    ovm.setLapiz()
+                    ovm.setSum(ovm.lapiz.value)
+                    expanded = false
+                }) {
+                    Text(text = "Lapiz $10")
+                }
+                DropdownMenuItem(onClick = {
+                    ovm.setBorrador()
+                    ovm.setSum(ovm.borrador.value)
+
+                    expanded = false
+                }) {
+                    Text(text = "Borrador $5")
+                }
+                DropdownMenuItem(onClick = {
+                    ovm.setLapicero()
+                    ovm.setSum(ovm.lapicero.value)
+
+                    expanded = false
+                }) {
+                    Text(text = "Lapicero $15")
+                }
+                DropdownMenuItem(onClick = {
+                    ovm.setCuaderno()
+                    ovm.setSum(ovm.cuaderno.value)
+
+                    expanded = false
+                }) {
+                    Text(text = "Cuaderno $30")
+                }
+            }
+        }
+    }
+}
+
 
     //Vista previa
-    @Preview(showSystemUi= true)
-    @Composable
-    fun Preview() {
-        ViewPractice()
-    }
+//    @Preview(showSystemUi= true)
+//    @Composable
+//    fun Preview() {
+//        ViewPractice()
+//    }
 
 
 
 
 
-}
+
+
 
 
 
